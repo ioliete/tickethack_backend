518462e7ed89c1a2e47da8922b442e0b70a8689a
Ines <ines.oliete@gmail.com>
2023-06-27 12:03:07 +0200

    url mongodb

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = '';
+const connectionString = 'mongodb+srv://admin:xCsI2lmcEsCLaMrh@admin.ex14vjt.mongodb.net/tickethack';
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
